b6912f03bc3ef81d74e67e4e3bc8a8f309bc173b
Patrick O Neill <20085099@wit.ie>
2020-11-26 17:49:03 +0000

    added routing for add,update,delete movie

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
@@ -8,6 +9,9 @@ const app = express();
 
 const port = process.env.PORT;
 
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
 
