1b4caa2553d62e94eb56c30d5a19b2dbcf494ceb
Patrick O Neill <20085099@wit.ie>
2020-12-07 14:21:05 +0000

    Week 9 Users Router

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -9,8 +9,8 @@ router.get('/', (req, res) => {
 });
 
 // register
-router.post('/', (req, res) => {
-        new User(req.body).save().then(user => res.status(200).json({success:true,token:"FakeTokenForNow"}))
+router.post('/', (req, res ,next) => {
+  User.create(req.body).then(user => res.status(200).json({success:true,token:"FakeTokenForNow"})).catch(next);
 });
 
 // Update a user
